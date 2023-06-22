0d5b7aa9e1dfdee5958ffdde83aeb10b56af4585
Gopher-Ninja <42231400+gopher-ninja@users.noreply.github.com>
2023-06-22 22:44:49 +0530

    Update attendance.vb

--- attendance.vb
+++ attendance.vb
@@ -3,7 +3,7 @@ Imports System.IO
 Imports System.Data
 Imports System.Data.SqlClient
 Public Class Employee_Attendance
-  Dim connection As SqlConnection = New SqlConnection("Data Source=Enter connection string here")
+    Dim connection As SqlConnection = New SqlConnection("Data Source=ROYAL\MSSQLSERVER04;Initial Catalog=payaldb;Integrated Security=True")
 
 
     Private Sub Employee_Attendance_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
@@ -13,32 +13,42 @@ Public Class Employee_Attendance
     End Sub
 
     Private Sub btn_open_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_open.Click
-        'query is to fetch data emp_name from sql  
+
+
+        ' Step-1: Check data is available for SELECTED DATE in emp_attendance table
+        ' Step-2: If data is present in emp_attendance table then just data from emp_attendance table for selected date
+        ' Step-3: If data is NOT present in emp_attendance then perform Step-4
+        ' Step-4: Get(Select statement) Emp_Name data from emp_reg table in that way we will get unique emp_Name data ( As in emp_attendance table Emp_Name is repeated)
+        ' Step-5: Visualize data in datagridview with present default checked
+
         connection.Open()
 
         ' Check if the data for that selected date is present in emp_attendance table or not present
 
-        Dim cmd As New SqlCommand("select count(*) from emp_attendance where Date=@Date", connection)
+        Dim cmd As New SqlCommand("select Id, Emp_Name, Status from emp_attendance where Date=@Date", connection)
         Dim adptor As New SqlDataAdapter(cmd)
         cmd.Parameters.Add(New SqlParameter With {.ParameterName = "@Date", .SqlDbType = SqlDbType.Date, .Value = DateTimePicker1.Value})
         Dim DT As New DataTable
         adptor.Fill(DT)
+        ' Clear old data present in datagridview1
         DataGridView1.Columns.Clear()
 
-        If DT.Rows(0)(0) <> 0 Then
+        ' Check if the row count is greater than 0 that means data is present for selected date in emp_attendance table
+        If DT.Rows.Count > 0 Then
+
+            ' As data is present in emp_attendance then we will show data from emp_attendance table for selected date
+            ' TO Restrict user to edit any data set read only  = true
             DataGridView1.ReadOnly = True
             ' If data is present show data to user from emp_attendance
-            Dim cmd2 As New SqlCommand("select Id, Emp_Name, Status from emp_attendance where Date=@Date", connection)
-            cmd2.Parameters.Add(New SqlParameter With {.ParameterName = "@Date", .SqlDbType = SqlDbType.Date, .Value = DateTimePicker1.Value})
-            Dim dbAdopter As New SqlDataAdapter(cmd2)
-            Dim dataSet As New DataSet()
-            dbAdopter.Fill(dataSet)
-            DataGridView1.DataSource = dataSet.Tables(0)
+            ' data will get filled in dbAdapter
+            ' Data will get visualize in DataGridView1 using following step (setting data source)
+            DataGridView1.DataSource = DT.Rows
             btn_submit.Enabled = False
 
         Else
+            ' Data is not present in emp_attendance so we will fetch data(Emp_Name) from emp_reg
             ' If data is not present show data to user from emp_reg
-            DataGridView1.ReadOnly = False
+            DataGridView1.ReadOnly = False ' So that user can edit the data P A L
             Dim cmd2 As New SqlCommand("select Id, Emp_Name from emp_reg", connection)
             Dim dbAdopter As New SqlDataAdapter(cmd2)
             Dim dataSet As New DataSet()
@@ -46,6 +56,7 @@ Public Class Employee_Attendance
             DataGridView1.ClearSelection()
             DataGridView1.DataSource = dataSet.Tables(0)
 
+            ' Create a column for present
             Dim checkBoxColumnPresent As New DataGridViewCheckBoxColumn()
             checkBoxColumnPresent.HeaderText = "Present"
             checkBoxColumnPresent.Width = 30
@@ -53,29 +64,36 @@ Public Class Employee_Attendance
             checkBoxColumnPresent.TrueValue = True
             DataGridView1.Columns.Insert(2, checkBoxColumnPresent)
 
+            ' Create a column for absent
             Dim checkBoxColumnAbsent As New DataGridViewCheckBoxColumn()
             checkBoxColumnAbsent.HeaderText = "Absent"
             checkBoxColumnAbsent.Width = 30
             checkBoxColumnAbsent.Name = "Absent"
             DataGridView1.Columns.Insert(3, checkBoxColumnAbsent)
+
+            ' Create a column for Leave
             Dim checkBoxColumnLeave As New DataGridViewCheckBoxColumn()
             checkBoxColumnLeave.HeaderText = "Leave"
             checkBoxColumnLeave.Width = 30
             checkBoxColumnLeave.Name = "Leave"
             DataGridView1.Columns.Insert(4, checkBoxColumnLeave)
+
+            ' Enable Submit button
             btn_submit.Enabled = True
+
+            ' To restrict user from adding and deleting row
             DataGridView1.AllowUserToAddRows = False
-            If DataGridView1.RowCount <> 0 Then
+            DataGridView1.AllowUserToDeleteRows = False
+
+            'To make "Present" cell "Checked" we will have to check each rows of the data grid view
+            If DataGridView1.RowCount > 0 Then
                 For Each row As DataGridViewRow In DataGridView1.Rows
-                    'Dim chkbox As DataGridViewCheckBoxCell = TryCast(row.Cells("Present"), DataGridViewCheckBoxCell)
-
-                    'chkbox.Value = headerChk.AutoCheck
-
+                    'To make "Present" cell "Checked" by setting its value True
                     DataGridView1.Item(2, row.Index).Value = True
                 Next
             End If
         End If
- 
+
         connection.Close()
 
     End Sub
@@ -86,39 +104,42 @@ Public Class Employee_Attendance
     End Sub
 
     Private Sub btn_submit_Click(sender As System.Object, e As System.EventArgs) Handles btn_submit.Click
+        ' To make sure atleast 1 of P, A or L should checked
         For Each row As DataGridViewRow In DataGridView1.Rows
-            Dim present As Boolean = CBool(row.Cells("Present").Value)
-            Dim absent As Boolean = CBool(row.Cells("Absent").Value)
-            Dim leave As Boolean = CBool(row.Cells("Leave").Value)
+            Dim present As Boolean = CBool(row.Cells("Present").Value)    ' Convert present cell value into boolean
+            Dim absent As Boolean = CBool(row.Cells("Absent").Value) ' Convert absent cell value into boolean
+            Dim leave As Boolean = CBool(row.Cells("Leave").Value) ' Convert leave cell value into boolean
+            ' If all are unchecked then give warning to user
             If present = False And absent = False And leave = False Then
-                MessageBox.Show("Please select valid value")
+                MessageBox.Show("Please select at least one checkbox")
                 Exit Sub
 
             End If
         Next
 
         connection.Open()
+        ' Inserting data into table for each row after PROPER VALIDATION 
         For Each row As DataGridViewRow In DataGridView1.Rows
             Dim present As Boolean = CBool(row.Cells("Present").Value)
             Dim absent As Boolean = CBool(row.Cells("Absent").Value)
             Dim leave As Boolean = CBool(row.Cells("Leave").Value)
-            Dim cmd2 As New SqlCommand("insert into emp_attendance(Id,Emp_Name,Date,Status) values(@Id,@Emp_Name,@Date,@Status)", connection)
-            cmd2.Parameters.AddWithValue("Id", row.Cells("Id").Value)
-            cmd2.Parameters.AddWithValue("Emp_Name", row.Cells("Emp_Name").Value)
-
-
+            Dim insertCmd As New SqlCommand("insert into emp_attendance(Id,Emp_Name,Date,Status) values(@Id,@Emp_Name,@Date,@Status)", connection)
+      
             If present Then
-                cmd2.Parameters.AddWithValue("Status", "Present")
+                insertCmd.Parameters.AddWithValue("Status", "Present")
             End If
             If absent Then
-                cmd2.Parameters.AddWithValue("Status", "Absent")
+                insertCmd.Parameters.AddWithValue("Status", "Absent")
             End If
             If leave Then
-                cmd2.Parameters.AddWithValue("Status", "Leave")
+                insertCmd.Parameters.AddWithValue("Status", "Leave")
             End If
-            cmd2.Parameters.Add(New SqlParameter With {.ParameterName = "@Date", .SqlDbType = SqlDbType.Date, .Value = DateTimePicker1.Value})
+            ' To add value for query 
+            insertCmd.Parameters.AddWithValue("Id", row.Cells("Id").Value)
+            insertCmd.Parameters.AddWithValue("Emp_Name", row.Cells("Emp_Name").Value)
+            insertCmd.Parameters.Add(New SqlParameter With {.ParameterName = "@Date", .SqlDbType = SqlDbType.Date, .Value = DateTimePicker1.Value})
 
-            cmd2.ExecuteNonQuery()
+            insertCmd.ExecuteNonQuery()
         Next
         connection.Close()
         MessageBox.Show("Data inserted Successfully")
